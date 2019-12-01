ac40be78fa11d107394a263863ed980aa5811163
tearill <1105317666@qq.com>
2019-12-01 14:52:03 +0800

    简单正则版本

---
+++ Printer Errors/a.js
@@ -0,0 +1,22 @@
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will output the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+// #Examples:
+
+// s="aaabbbbhaijjjm"
+// error_printer(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// error_printer(s) => "8/22"
+function printerError(s) {
+    // your code
+    let len = s.length;
+    let count = (s.match(/[abcdefghijklm]/ig)||[]).length;
+    return `${len-count}/${len}`;
+}
+console.log(printerError("aaabbbbhaijjjm"));
