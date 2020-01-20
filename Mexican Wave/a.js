5d3823185291d5c8d1c849d924333a9b3ada5c03
tearill <1105317666@qq.com>
2020-01-20 21:05:52 +0800

    Mexican Wave

--- Mexican Wave/a.js
+++ Mexican Wave/a.js
@@ -1,7 +1,7 @@
 function wave(str) {
     var res = [];
     str.split('').forEach((val, index) => {
-        res.push(str.slice(0, index) + val.toUpperCase() +str.slice(index + 1));
+        res.push(str.slice(0, index) + val.toUpperCase() + str.slice(index + 1));
     });
     return str;
 }
